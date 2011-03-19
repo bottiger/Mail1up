994c5b1b1d603ef90607f71402d440af5b0464bc
Johan Brinch <brinchj@gmail.com>
2011-03-19 16:26:01 +0100

    Add hash key index

---
+++ test.erl
@@ -0,0 +1,37 @@
+-module(test).
+-export([build/1, search/1]).
+
+build(0, F) -> file:close(F), F;
+build(N, F) ->
+    Key = crypto:sha(integer_to_list(N)),
+    file:write(F, Key),
+    build(N-1, F).
+
+build(N) ->
+    {ok, Device} = file:open("data.dat", [write]),
+    build(N, Device).
+
+search_block(Key, Block) ->
+    case Block of
+        <<K:20/binary, R/binary>> ->
+            case K of
+                Key -> found;
+                _ -> search_block(Key, R)
+            end;
+        _ -> not_found
+    end.
+
+search(Device, Key) ->
+    case file:read(Device, 16380) of
+        {ok, Data} ->
+            case search_block(Key, Data) of
+                found -> found;
+                not_found -> search(Device, Key)
+            end;
+        eof -> not_found
+    end.
+
+search(N) ->
+    {ok, Device} = file:open("data.dat", [read, binary]),
+    Key = crypto:sha(integer_to_list(N)),
+    search(Device, Key).
