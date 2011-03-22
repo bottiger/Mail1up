8cf752ce1845d9d84ec7cd043fbaf6ddcd9d45f0
Johan Brinch <brinchj@gmail.com>
2011-03-22 16:18:21 +0100

    Rewrite for argparse (Python 3.2), enable stdin option for --username and --password, add --humanize

--- imap.py
+++ imap.py
@@ -1,76 +1,82 @@
 #!/usr/bin/env python3
 
 import imaplib
-import getopt
-import sys
 import json
+import getpass
+from argparse import ArgumentParser
 
-username = "jmailbackup44@gmail.com"
-password = "qwerty60"
-server = "imap.gmail.com"
-folder = "INBOX"
-headers_only = False
-msg_id = 0
-
-opts, remainder = getopt.getopt(sys.argv[1:], 'u:p:s:f:a:',
-                                ['username=',
-                                 'password=',
-                                 'server=',
-                                 'folder=',
-                                 'after=',
-                                 'headers-only',
-                                 'msg-id=',
-                                 ])
-
-for opt, arg in opts:
-        if opt in ("-u", "--username"):
-            username = arg
-        elif opt in ("-p", "--password"):
-            password = arg
-        elif opt in ("-s", "--server"):
-            server = arg
-        elif opt in ("-f", "--folder"):
-            folder = arg
-        elif opt in ("a", "--after"):
-            start_date = arg
-        elif opt in ("--headers-only"):
-            headers_only = True
-        elif opt in ("--msg-id"):
-            msg_id = str(int(arg))
-
-if headers_only:
-    message_parts = "HEADER"
-else:
-    message_parts = "TEXT"
+# List of arguments for ArgumentParser
+arguments = [
+    ('username', dict(default='jmailbackup44@gmail.com')),
+    ('password', dict(default='qwerty60')),
+    ('host', dict(default='imap.gmail.com')),
+    ('folder', dict(default='INBOX')),
+    ('after', dict(type=str)),
+    ('headers-only', dict(default=False, action='store_const', const=True)),
+    ('humanize', dict(default=False, action='store_const', const=True)),
+    ('message-id', dict(type=int)),
+    ]
 
 
-def fetch_mail(M, msg_id, msg_parts, mail_list):
-    typ, data = M.fetch(msg_id, '(BODY.PEEK[' + msg_parts + '])')
+def unpack_argument(args):
+    ''' Convenient argument wrapper '''
+    if not 'const' in args:
+        if 'default' in args and not 'type' in args:
+            # find type from default value
+            args['type'] = type(args['default'])
+    return args
+
+# Build argument parser
+parser = ArgumentParser(description='IMAP wrapper.')
+for name, args in arguments:
+    args = unpack_argument(args)
+    print(args)
+    parser.add_argument('--%s' % name,
+                        dest=name.replace('-', '_'),
+                        **args)
+args = parser.parse_args()
+
+# Set message part
+message_part = 'TEXT'
+if args.headers_only:
+    message_part = "HEADER"
+
+
+def fetch_mail(M, msg_id, msg_part):
+    ''' Fetch mail content '''
+    typ, data = M.fetch(str(int(msg_id)), '(BODY.PEEK[' + msg_part + '])')
     data2 = data[0][1].decode()
-    mail_list.append({"mail-id": str(int(msg_id)), "content": data2})
-    return mail_list
+    return {"mail-id": str(int(msg_id)), "content": data2}
 
 
-M = imaplib.IMAP4_SSL(server)
-M.login(username, password)
-M.list
-M.select(folder, "true")
+# Init mailbox
+M = imaplib.IMAP4_SSL(args.host)
+# Get username and password
+usernm = args.username
+usernm = (usernm == '-' and getpass.getpass('Username: ')) or usernm
+passwd = args.password
+passwd = (passwd == '-' and getpass.getpass()) or passwd
+M.login(usernm, passwd)
+M.select(args.folder, "true")
 typ, data = M.search(None, 'ALL')
 
 mail_list = []
 
-if msg_id:
-    mail_list = fetch_mail(M, msg_id, message_parts, mail_list)
+if args.message_id is not None:
+    mail_list.append(fetch_mail(M, args.message_id, message_part))
 else:
     for num in data[0].split():
-        mail_list = fetch_mail(M, num, message_parts, mail_list)
+        mail_list.append(fetch_mail(M, num, message_part))
         ##typ, data = M.fetch(num, '(BODY.PEEK[' + message_parts + '])')
         ##data2 = data[0][1].decode()
         ##mail_list.append({"mail-id": str(int(num)), "headers": data2})
         #mail_list.append({int(num): data2})
         #print('%s\n' % data[0][1].decode())
 
-print(json.dumps(mail_list, separators=(',', ':')))
+if args.humanize:
+    print(json.dumps(mail_list, indent=True))
+else:
+    print(json.dumps(mail_list, separators=(',', ':')))
 
 M.close()
 M.logout()
