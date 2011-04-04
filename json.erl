ad00fb7e33ce5e5f96e9e55fe783f9d6822e9c90
Johan Brinch <brinchj@gmail.com>
2011-04-04 14:20:34 +0200

    Strict JSON parser after RFC-4627

--- json.erl
+++ json.erl
@@ -1,5 +1,9 @@
 -module(json).
--export([term_to_json/1]).
+-export([term_to_json/1, json_to_term/1, read_number/1]).
+
+
+json_to_term(JSON) ->
+    json_parser:parse(JSON).
 
 
 to_string(S) when is_list(S) ->
@@ -39,34 +43,61 @@ quote(S) ->
 
 %% Term to JSON
 term_to_json(true) ->
-    "true";
+    {ok, "true"};
 
 term_to_json(false) ->
-    "false";
+    {ok, "false"};
 
 term_to_json(null) ->
-    "null";
+    {ok, "null"};
 
 term_to_json(I) when is_integer(I) ->
-    integer_to_list(I);
+    {ok, integer_to_list(I)};
 
 term_to_json(F) when is_float(F) ->
-    float_to_list(F);
+    {ok, float_to_list(F)};
 
 term_to_json(B) when is_binary(B) ->
-    quote(binary_to_list(B));
+    {ok, quote(binary_to_list(B))};
 
 term_to_json(L) when is_list(L) ->
-    Vals = lists:map(fun(X) -> term_to_json(X) end, L),
-    "[" ++ string:join(Vals, ",") ++ "]";
+    Vals = lists:map(fun(X) ->
+                             {ok, JSON} = term_to_json(X),
+                             JSON
+                     end, L),
+    {ok, "[" ++ string:join(Vals, ",") ++ "]"};
 
 term_to_json(D) when is_tuple(D) ->
     Lst = dict:to_list(D),
     Vals = lists:map(fun({Key, Val}) ->
-                             quote(to_string(Key)) ++ ":" ++ term_to_json(Val) end, Lst),
-    "{" ++ string:join(Vals, ",") ++ "}".
+                             {ok, JSON} = term_to_json(Val),
+                             quote(to_string(Key)) ++ ":" ++ JSON
+                     end, Lst),
+    {ok, "{" ++ string:join(Vals, ",") ++ "}"}.
 
 
 
 %% JSON to Term
+read_number("", {int, S, N}) ->
+    {ok, list_to_integer(S ++ lists:reverse(N)), ""};
+read_number("", {float, S, F}) ->
+    {ok, list_to_float(S ++ lists:reverse(F)), ""};
 
+read_number([C | Rest], {T, S, [D|Acc]}) when $0 =< C andalso C =< $9 ->
+    read_number(Rest, {T, S, [C,D|Acc]});
+
+read_number([C | Rest], {int, S, ""}) when $1 =< C andalso C =< $9 ->
+    read_number(Rest, {int, S, [C]});
+
+read_number([$0, $. | Rest], {int, S, ""}) ->
+    io:format(Rest, []),
+    read_number(Rest, {float, S, ".0"});
+
+read_number([$. | Rest], {int, S, [D|Acc]}) ->
+    read_number(Rest, {float, S, [$.,D|Acc]});
+
+read_number([$- | Rest], {int, "", ""}) ->
+    read_number(Rest, {int, "-", ""}).
+
+read_number(S) ->
+    read_number(S, {int, "", ""}).
