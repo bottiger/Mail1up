dbcc1529168b050b0f5dbac49d6e6b52799e8127
Arvid Böttiger <bottiger@gmail.com>
2011-04-11 10:54:26 +0200

    quickfix

--- src/mailget.erl
+++ src/mailget.erl
@@ -2,17 +2,32 @@
 -compile(export_all).
 -export([check/0, check/1, check/2, check/3]).
 
--spec list_foldes(string(), string()) -> list(string()).
+-spec list_folders(string(), string()) -> list(string()).
 list_folders(User, Pass) ->
     % ImapFolders is a list of Binary objects like:
     % <<"(\\HasNoChildren) \"/\" \"Personal\"">>
     {ok, ImapFolders} = imappy(User, Pass, ["--list-folders"]),
     lists:map(fun(X) -> lists:last(string:tokens(binary:bin_to_list(X), "\"")) end, ImapFolders).
     
--spec mail_id(string(), string(), Integer) -> list(string()).
+-spec mail_id(string(), string(), integer()) -> list(string()).
+mail_id(User, Pass, Id, Folder) ->
+    {ok, MailDict} = imappy(User, Pass, ["--message-id", integer_to_list(Id), "--folder", Folder]),
+    %tring:tokens(binary:bin_to_list(Mail), "\r\n").
+    NewMailDict = dict:new(),
+    dict:map(fun(K, V) -> dict:store(binary:bin_to_list(K), binary:bin_to_list(V), NewMailDict) end, MailDict),
+    NewMailDict.
+
+-spec mail_id(string(), string(), integer()) -> 
 mail_id(User, Pass, Id) ->
-    {ok, Mail} = imappy(User, Pass, ["--message-id", integer_to_list(Id)]),
-    string:tokens(binary:bin_to_list(Mail), "\r\n").
+    mail_id(User, Pass, Id, "INBOX").
+
+mail_header_id(User, Pass, Id) ->
+    ok.
+
+parse_mail_dict(MailDict) ->
+    NewMailDict = dict:new(),
+    dict:map(fun(K, V) -> dict:store(binary:bin_to_list(K), binary:bin_to_list(V), NewMailDict) end, MailDict),
+    NewMailDict.
 
 -spec check() -> term().
 check() -> check("").
