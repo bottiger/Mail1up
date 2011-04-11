cde270b6f848cd2d1f27c050e62e6959747aded1
Arvid Böttiger <bottiger@gmail.com>
2011-04-11 13:21:15 +0200

    Added get_mails() and get_headers() to mailget.erl

--- src/mailget.erl
+++ src/mailget.erl
@@ -11,6 +11,25 @@ list_folders(User, Pass) ->
     {ok, ImapFolders} = imappy(User, Pass, ["--list-folders"]),
     lists:map(fun(X) -> lists:last(string:tokens(binary:bin_to_list(X), "\"")) end, ImapFolders).
 
+%% @doc Returns a dict with Key => mail-id and Value = MailContent
+%% @end
+get_mails(User, Pass) ->
+    {ok, Mails} = imappy(User, Pass),
+    parse_mail_list(Mails).
+
+%% @doc Returns a dict with Key => mail-id and Value = Headers
+%% @end
+get_headers(User, Pass) ->
+    {ok, Headers} = imappy(User, Pass, ["--headers-only"]),
+    parse_mail_list(Headers).
+
+%% @doc Returns a dict with Key => mail-id and Value => (A dict where Key => Header and Value => Header Value)
+%% For instance: Key => "Date" and Value => "Fri, 18 Mar 2011 08:49:18 -0700"
+%% @end
+get_headers_dict(User, Pass) ->
+    HeaderDict = get_headers(User, Pass),
+    dict:fold(fun(K, V, AccIn) -> dict:store(K, header_to_dict(V), AccIn) end, dict:new(), HeaderDict).
+
 %-spec mail_id(string(), string(), integer()) ->
 mail_id(User, Pass, Id) ->
     mail_id(User, Pass, Id, "INBOX").
@@ -31,20 +50,35 @@ mail_header_id(User, Pass, Id) ->
 mail_header_id_dict(User, Pass, Id) ->
     MailDict = mail_header_id(User, Pass, Id),
     HeaderString = dict:fetch("content", MailDict),
-    HeaderList = string:tokens(HeaderString, "\r\n"), % Gmail uses \r\n instead of \n. What should we split at?
-    dict:from_list(lists:map(fun(X) -> 
+    header_to_dict(HeaderString).
+
+%-spec parse_mail_dict(dict()) -> dict().
+%% @doc Converts MailDict from a Dict of <<"binaries">> to utf-8">> into a dict of "utf8 strings"
+%% @end
+parse_mail_dict(MailDict) ->
+    dict:fold(fun(K, V, AccIn) -> dict:store(unicode:characters_to_list(K), unicode:characters_to_list(V), AccIn) end, dict:new(), MailDict).
+
+%% @doc Transform a list of E-mails returned from imappy into a dict where Key = mail-id and Value = MailContent 
+%% @end
+parse_mail_list(MailList) ->
+    lists:foldl(fun(M, AccIn) ->
+        ParsedMail = parse_mail_dict(M),
+        Key = list_to_integer(dict:fetch("mail-id", ParsedMail)),
+        Value = dict:fetch("content", ParsedMail),
+        dict:store(Key, Value, AccIn)
+        end, dict:new(), MailList).
+
+%% @doc Transform the raw headers into a dict where Key => Header and Value => Headers Value
+%% For instance: Key => "Date" and Value => "Fri, 18 Mar 2011 08:49:18 -0700"
+%% @end
+header_to_dict(Header) ->
+    HeaderList = string:tokens(Header, "\r\n"), % Gmail uses \r\n instead of \n. What should we split at?
+    dict:from_list(lists:map(fun(X) ->
                 Parts = string:tokens(X,":"),
                 Key = lists:nth(1, Parts),
                 Value = string:strip(string:join(lists:nthtail(1, Parts), ":")),
                 {Key, Value}
               end, HeaderList)).
-
-
-%-spec parse_mail_dict(dict()) -> dict().
-%% @doc Converts MailDict from a Dict of <<"binaries">> th utf-8">> into a dict of "utf8 strings"
-%% @end
-parse_mail_dict(MailDict) ->
-    dict:fold(fun(K, V, AccIn) -> dict:store(unicode:characters_to_list(K), unicode:characters_to_list(V), AccIn) end, dict:new(), MailDict).
 
 -spec check() -> term().
 check() -> check("").
@@ -56,6 +90,9 @@ check(User, Pass, MsgID) when is_integer(MsgID) ->
 
 check(User, Pass, MsgArgs) ->
     imappy(User, Pass, MsgArgs).
+
+imappy(User, Pass) ->
+    imappy(User, Pass, "").
 
 -spec imappy(list(), list(), list()) -> list().
 imappy(User, Pass, MsgArgs) ->
